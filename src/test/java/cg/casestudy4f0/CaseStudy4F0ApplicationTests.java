f6059a5ad5ed7e52b13e4c82cf5bafe948c3e0a3
DuongVanTrung31 <trung.duong.dt31@gmail.com>
2022-03-13 20:46:19 +0700

    init project

---
+++ src/test/java/cg/casestudy4f0/CaseStudy4F0ApplicationTests.java
@@ -0,0 +1,13 @@
+package cg.casestudy4f0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaseStudy4F0ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
