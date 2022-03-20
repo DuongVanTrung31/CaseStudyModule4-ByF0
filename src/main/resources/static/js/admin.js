ea03397013cf844b1a865ae8f7d639f58dfba081
cuongkaren <quoccuongd16cn2@gmail.com>
2022-03-20 12:28:03 +0700

    Thêm file

--- src/main/resources/static/js/admin.js
+++ src/main/resources/static/js/admin.js
@@ -141,7 +141,7 @@ function deleteProduct(id) {
 //hàm hiển thị table product data
 function displayProduct(product) {
     return `<tr><td>${product.name}</td><td>${product.price}</td><td>${product.quantity}</td><td>${product.weight}</td>
-            <td>${product.description}</td><td><img src="../static/images/${product.image}" alt="Lỗi"></td><td>${product.category.name}</td>
+            <td>${product.description}</td><td><img src="../static/img/${product.image}" alt="Lỗi"></td><td>${product.category.name}</td>
             <td><button class="btn btn-danger" onclick="deleteProduct(${product.id})">Delete</button></td>
             <td><button class="btn btn-warning" onclick="editProduct(${product.id})">Edit</button></td></tr>`;
 }
